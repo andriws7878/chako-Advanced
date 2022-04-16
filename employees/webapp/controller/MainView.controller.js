b5bb5fe01eb2b39e117d64df5bc3dac46cfb80aa
andriws7878 <andriws7878@gmail.com>
2022-04-16 22:07:42 +0000

    02 Elementos Controladores

--- employees/webapp/controller/MainView.controller.js
+++ employees/webapp/controller/MainView.controller.js
@@ -1,15 +1,27 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
-    /**
+	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
     function (Controller) {
         "use strict";
 
-        return Controller.extend("chakoapp.employees.controller.MainView", {
-            onInit: function () {
+        var Main = Controller.extend("andriws.Employees.controller.MainView", {});
 
-            }
-        });
+            Main.prototype.onValidate = function () {
+                var inputEmpleyee = this.byId("inputEmpleyee");
+                var valueEmpleyee = inputEmpleyee.getValue();
+
+                if (valueEmpleyee.length === 6) {
+                    //inputEmpleyee.setDescription("OK");
+                    this.getView().byId("labelCountry").setVisible(true);
+                    this.getView().byId("slCountry").setVisible(true);
+                } else {
+                    //inputEmpleyee.setDescription("Not OK");
+                    this.getView().byId("labelCountry").setVisible(false);
+                    this.getView().byId("slCountry").setVisible(false);
+                };
+            };
+        return Main;
     });
